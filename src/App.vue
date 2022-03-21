7294ca0ec608b98e1e7e255293b69de77bc4dd2d
Nicolas de Garrigues <ndegarrigues@gmail.com>
2022-03-21 14:28:30 +0100

    :sparkles: FEAT layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <AppLayout>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </AppLayout>
 </template>
+
+<script>
+import AppLayout from '@/layouts/AppLayout.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppLayout,
+  },
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <header>
+            <NavbarLayout />
+        </header>
+        <main>
+            <slot></slot>
+        </main>
+    </div>
+</template>
+
+<script>
+    import NavbarLayout from '@/components/includes/Navbar.vue';
+
+    export default {
+        name: 'AppLayout',
+        components: {
+            NavbarLayout,
+        },
+    }
+</script>
