556e11f2a84e8dcb2df35af3efa3144624396146
Nicolas de Garrigues <ndegarrigues@gmail.com>
2022-03-21 17:12:57 +0100

    :sparkles: FEAT sign In function

--- src/components/includes/Navbar.vue
+++ src/components/includes/Navbar.vue
@@ -4,18 +4,23 @@
       <router-link :to="{name: 'home'}" class="cursor-pointer text-2xl font-bold leading-7 text-white sm:text-3xl sm:truncate">Instagrave</router-link>
     </div>
     <div class="mt-4 flex md:mt-0 md:ml-4">
-      <router-link :to="{name: 'signIn'}" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">Sign In</router-link>
-      <router-link :to="{name: 'signUp'}" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">Sign Up</router-link>
+      <div v-if="user_data.role">
+        <router-link :to="{name: 'signIn'}" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">Catalogue</router-link>
+      </div>
+      <div v-else>
+        <router-link :to="{name: 'signIn'}" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">Sign In</router-link>
+        <router-link  ter-link :to="{name: 'signUp'}" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">Sign Up</router-link>
+      </div>
     </div>
   </div>
 </template>
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: 'NavbarLayout',
-  methods: {
-    goTo(e) {
-      this.$router.push({name: e});
-    },
+  computed: {
+    ...mapState('user', ['user_data']),
   },
 }
 </script>
--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,65 +1,67 @@
 <template>
    <div class="bg-grey-lighter min-h-screen flex flex-col">
-            <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                    <h1 class="mb-8 text-3xl text-center">Sign In</h1>
+        <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+            <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                <h1 class="mb-8 text-3xl text-center">Sign In</h1>
 
-                    <input
-                        id="email"
-                        v-model="email"
-                        type="text"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="email"
-                        placeholder="Email" />
+                <input
+                    id="email"
+                    v-model="email"
+                    type="text"
+                    class="block border border-grey-light w-full p-3 rounded mb-4"
+                    name="email"
+                    placeholder="Email" />
 
-                    <input 
-                        id="password"
-                        v-model="password"
-                        type="password"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="password"
-                        placeholder="Password" />
-                    <button
-                        @click="verifyUser()"
-                        type="submit"
-                        class="w-full text-center py-3 rounded bg-black text-white hover:bg-green-dark focus:outline-none my-1"
-                    >Sign In</button>
+                <input 
+                    id="password"
+                    v-model="password"
+                    type="password"
+                    class="block border border-grey-light w-full p-3 rounded mb-4"
+                    name="password"
+                    placeholder="Password" />
+                <button
+                    @click="signUser()"
+                    type="submit"
+                    class="w-full text-center py-3 rounded bg-black text-white hover:bg-green-dark focus:outline-none my-1"
+                >Sign In</button>
 
 
-                </div>
+            </div>
 
-                <div class="text-grey-dark mt-6">
-                    Don't have an account? 
-                    <router-link :to="{name: 'signUp'}" class="no-underline border-b border-blue text-blue">
-                        Sign In
-                    </router-link>.
-                </div>
+            <div class="text-grey-dark mt-6">
+                Don't have an account? 
+                <router-link :to="{name: 'signUp'}" class="no-underline border-b border-blue text-blue">
+                    Sign In
+                </router-link>.
             </div>
         </div>
+    </div>
 </template>
 <script>
-    import { supabase } from "@/supabase"
+import { mapActions } from "vuex";
+import { supabase } from "@/supabase";
 
-    export default {
-        name: 'SignIn',
-        data() {
-            return {
-                email: '',
-                password: '',
-            }
-        },
-        methods: {
-            async verifyUser() {
-                const {data: user, error} = await supabase.auth.signIn({
-                    email: this.email,
-                    password: this.password,
-                })
-                if (user) {
-                    console.log(user, 'user')
-                } else {
-                    console.log(error, 'error')
-                }
-            }
-        },
+export default {
+  data(){
+    return{
+      email: "",
+      password: "",
     }
+  },
+  methods: {
+    ...mapActions('user', ["setUser"]),
+    async signUser() {
+      const { error, user } = await supabase.auth.signIn({
+        email: this.email,
+        password: this.password,
+      });
+      if (user) {
+        this.setUser(user);
+      }
+      else if (error) {
+        console.log(error);
+      }
+	}
+    }
+}
 </script>
