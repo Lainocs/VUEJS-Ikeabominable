d23ec077dba9b6775c0a45509d5dfc2a8dc68262
Nicolas de Garrigues <ndegarrigues@gmail.com>
2022-03-21 16:38:11 +0100

    :sparkles: FEAT signIn & signUp

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,10 +1,65 @@
 <template>
-    <div>
-        SignIn
-    </div>
+   <div class="bg-grey-lighter min-h-screen flex flex-col">
+            <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                    <h1 class="mb-8 text-3xl text-center">Sign In</h1>
+
+                    <input
+                        id="email"
+                        v-model="email"
+                        type="text"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="email"
+                        placeholder="Email" />
+
+                    <input 
+                        id="password"
+                        v-model="password"
+                        type="password"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="password"
+                        placeholder="Password" />
+                    <button
+                        @click="verifyUser()"
+                        type="submit"
+                        class="w-full text-center py-3 rounded bg-black text-white hover:bg-green-dark focus:outline-none my-1"
+                    >Sign In</button>
+
+
+                </div>
+
+                <div class="text-grey-dark mt-6">
+                    Don't have an account? 
+                    <router-link :to="{name: 'signUp'}" class="no-underline border-b border-blue text-blue">
+                        Sign In
+                    </router-link>.
+                </div>
+            </div>
+        </div>
 </template>
 <script>
+    import { supabase } from "@/supabase"
+
     export default {
-        name: 'SignIn'
+        name: 'SignIn',
+        data() {
+            return {
+                email: '',
+                password: '',
+            }
+        },
+        methods: {
+            async verifyUser() {
+                const {data: user, error} = await supabase.auth.signIn({
+                    email: this.email,
+                    password: this.password,
+                })
+                if (user) {
+                    console.log(user, 'user')
+                } else {
+                    console.log(error, 'error')
+                }
+            }
+        },
     }
 </script>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,10 +1,65 @@
 <template>
-    <div>
-        SignUp
-    </div>
+   <div class="bg-grey-lighter min-h-screen flex flex-col">
+            <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                    <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+
+                    <input
+                        id="email"
+                        v-model="email"
+                        type="text"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="email"
+                        placeholder="Email" />
+
+                    <input 
+                        id="password"
+                        v-model="password"
+                        type="password"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="password"
+                        placeholder="Password" />
+                    <button
+                        @click="createUser()"
+                        type="submit"
+                        class="w-full text-center py-3 rounded bg-black text-white hover:bg-green-dark focus:outline-none my-1"
+                    >Create Account</button>
+
+
+                </div>
+
+                <div class="text-grey-dark mt-6">
+                    Already have an account? 
+                    <router-link :to="{name: 'signIn'}" class="no-underline border-b border-blue text-blue">
+                        Sign In
+                    </router-link>.
+                </div>
+            </div>
+        </div>
 </template>
 <script>
+    import { supabase } from "@/supabase"
+
     export default {
-        name: 'SignUp'
+        name: 'SignIn',
+        data() {
+            return {
+                email: '',
+                password: '',
+            }
+        },
+        methods: {
+            async createUser() {
+                const {data: user, error} = await supabase.auth.api.createUser({
+                    email: this.email,
+                    password: this.password,
+                })
+                if (user) {
+                    console.log(user, 'user')
+                } else {
+                    console.log(error, 'error')
+                }
+            }
+        },
     }
 </script>
